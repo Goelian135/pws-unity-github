e5a31297d239a4af99305410596fb4c32298c2ff
Julian van Kuik <118647@minkemacollege.nl>
2025-09-28 19:55:27 +0200

    Revert "gefaalde poging om dash te fixen"
    This reverts commit 787cb1fd5789be41a20dd54823f6422bb26eac57.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -11,42 +11,31 @@ public class PlayerMovement : MonoBehaviour
     public float runSpeed = 40f;
 
     bool jump = false;
-
-    public float dashCooldown = 0.3f;
-    public bool isDashing = false;
-    [SerializeField] private bool canDash = true;
+    bool dash = false;
 
     // Update is called once per frame
     void Update()
     {
-        //check if we can dash again
-        if (!canDash && !isDashing && Time.time > controller.dashTimeStart + controller.dashDuration + dashCooldown)
-        {
-            canDash = true;
-        }
-
         //get horizontal movement input
         horizontalMove = InputManager.Instance.GetKey("MoveLeft") ? -1 :
                          InputManager.Instance.GetKey("MoveRight") ? 1 : 0;
 
-        //check for jump input
         if (InputManager.Instance.GetKeyDown("Jump"))
         {
             jump = true;
         }
 
-        //check for dash input
-        if (InputManager.Instance.GetKeyDown("Dash") && canDash && !isDashing)
+        if (InputManager.Instance.GetKeyDown("Dash"))
         {
-            isDashing = true;
-            canDash = false;
+            dash = true;
         }
     }
 
     private void FixedUpdate()
     {
         //move the character
-        controller.Move(horizontalMove *runSpeed *Time.fixedDeltaTime, jump);
+        controller.Move(horizontalMove *runSpeed *Time.fixedDeltaTime, jump, dash);
         jump = false;
+        dash = false;
     }
 }
